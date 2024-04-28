b08f3c4d7913d7f3ace712dc15f96b69c6706780
zaker1998 <zaker1998@mail.ru>
2024-04-28 23:58:33 +0500

    parameters input for server testing

--- src/main/java/com/marat/spring/controller/FirstController.java
+++ src/main/java/com/marat/spring/controller/FirstController.java
@@ -3,12 +3,19 @@ package com.marat.spring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/first")
 public class FirstController {
     @GetMapping("/hello")
-    public String sayHelloPage(){
+    public String sayHelloPage(@RequestParam(value = "name", required = false) String name,
+                               @RequestParam(value = "surname", required = false) String surname){
+        if(!(name.equals("")) && !(surname.equals("")))
+        {
+            System.out.println(name + surname);
+            return "first/paramOut";
+        }
         return "first/hello";
     }
 
