07bfed6cef2baf703b2d035105fd502a58a4f482
zaker1998 <zaker1998@mail.ru>
2024-04-29 20:15:20 +0500

    person controller added

---
+++ src/main/java/com/marat/spring/controller/PeopleController.java
@@ -0,0 +1,21 @@
+package com.marat.spring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/people")
+public class PeopleController {
+    @GetMapping()
+    public String index(Model model) {
+        return null;
+    }
+
+    @GetMapping("/id")
+    public String show(@PathVariable("id") int id) {
+        return null;
+    }
+}
