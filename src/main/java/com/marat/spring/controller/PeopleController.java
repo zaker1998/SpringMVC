7a05c4894d10bfbfea315d52602aa7358c316cc6
zaker1998 <zaker1998@mail.ru>
2024-04-30 20:59:24 +0500

    controller methods filled up for indexing and showing of people in DAO

--- src/main/java/com/marat/spring/controller/PeopleController.java
+++ src/main/java/com/marat/spring/controller/PeopleController.java
@@ -1,5 +1,6 @@
 package com.marat.spring.controller;
 
+import com.marat.spring.dao.PersonDAO;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,13 +10,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/people")
 public class PeopleController {
+    private final PersonDAO personDAO;
+    public PeopleController(PersonDAO personDAO) {
+        this.personDAO = personDAO;
+    }
+
     @GetMapping()
     public String index(Model model) {
-        return null;
+        model.addAttribute("people", personDAO.index());
+        return "people/index";
     }
 
     @GetMapping("/id")
     public String show(@PathVariable("id") int id, Model model) {
-        return null;
+        model.addAttribute("person", personDAO.show(id));
+        return "people/show";
     }
 }
