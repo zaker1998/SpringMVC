38c79c1c47519b2ea6f1a14ff035ca224d70d175
zaker1998 <zaker1998@mail.ru>
2024-05-02 00:11:28 +0500

    html th:action error fixed, now addidtion of new PErson works as intended

--- src/main/java/com/marat/spring/models/Person.java
+++ src/main/java/com/marat/spring/models/Person.java
@@ -3,6 +3,7 @@ package com.marat.spring.models;
 public class Person {
     private int id;
     private String name;
+    public Person(){}
 
     public Person(int id, String name) {
         this.id = id;
