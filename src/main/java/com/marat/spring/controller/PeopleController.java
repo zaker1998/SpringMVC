8ead129fd25d252bab5cb9798097b2fe8d7dacbd
zaker1998 <zaker1998@mail.ru>
2024-05-03 20:09:21 +0500

    delete and patch http methods implemetation

--- src/main/java/com/marat/spring/controller/PeopleController.java
+++ src/main/java/com/marat/spring/controller/PeopleController.java
@@ -38,4 +38,16 @@ public class PeopleController {
         personDAO.save(person);
         return "redirect:/peopleList";
     }
+
+    @PatchMapping("/{id}")
+    public String update(@ModelAttribute("person") Person person, @PathVariable("id") int id) {
+        personDAO.update(id, person);
+        return "redirect:/people";
+    }
+
+    @DeleteMapping("/{id}")
+    public String delete(@PathVariable("id") int id) {
+        personDAO.delete(id);
+        return "redirect:/people";
+    }
 }
